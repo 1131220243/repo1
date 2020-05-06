9f8724b2f65b6c04967fd46b9269f0bfaf84e469
YuanFangMa <1131220243@qq.com>
2020-05-06 19:36:36 +0800

    我要删除这个工程

--- 01_Exception/src/com/itcast/exception/Demo07.java
+++
@@ -1,9 +0,0 @@
-package com.itcast.exception;
-
-public class Demo07 {
-    public static void main(String[] args) {
-        Demo06 r = new Demo06();
-        Thread thread = new Thread(r);
-        thread.start();
-    }
-}
--- 01_Exception/src/com/itcast/statics/Demo03.java
+++
@@ -1,9 +0,0 @@
-package com.itcast.statics;
-
-public class Demo03 {
-    public static void main(String[] args) {
-        int i = 1;
-        String s1 = String.valueOf(1);
-        System.out.println(s1);
-    }
-}
