4a91d8f11c790f3390313c4b38b9ce3d5408737e
YuanFangMa <1131220243@qq.com>
2020-05-23 14:59:43 +0800

    把工程添加到本地仓库

---
+++ gitTest/src/com/lll/sss/Demo.java
@@ -0,0 +1,13 @@
+package com.lll.sss;
+
+/**
+ * @author shkstart
+ * @create 2020-05-23-14:32
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("xxxx");
+        System.out.println("........");
+        System.out.println("cccc");
+    }
+}
